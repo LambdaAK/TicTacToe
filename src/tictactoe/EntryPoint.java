e0b31eeea3bd7171a9daeead312bde70eb6fa081
Alex Kozik <sous3-Bryson>
2021-11-26 04:51:41 -0500

    Added colored windows

--- src/tictactoe/EntryPoint.java
+++ src/tictactoe/EntryPoint.java
@@ -1,7 +1,14 @@
 package tictactoe;
 
+import javax.swing.UIManager;
+import java.awt.Color;
+
 public class EntryPoint {
     public static void main(String[] args) {
+		UIManager.put("OptionPane.messageForeground", Color.GREEN);
+		UIManager.put("Panel.background", Color.decode("222222"));
+        UIManager.put("OptionPane.background", Color.decode("222222"));
+        UIManager.put("Button.background", Color.GREEN);
         TicTacToeMenu menu = new TicTacToeMenu();
         menu.play();
         
